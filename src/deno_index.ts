fa10c7ba755f2c63793d8b1f5dac83ecddc68c64
wzgl2010 <wzgl2011@hotmail.com>
2025-10-31 18:46:16 +0800

    Update deno_index.ts

--- src/deno_index.ts
+++ src/deno_index.ts
@@ -2,7 +2,6 @@ import { handleRequest } from "./new_request.js";
 
 async function denoHandleRequest(req: Request): Promise<Response> {
   const url = new URL(req.url);
-  console.log('Request URL:', req.url);
   return handleRequest(req);
 };
 
